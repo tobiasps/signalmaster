996808c33a8e749c4617fb00c3af6bf90286fdaa
Tobias Pyndt Steinmann <tps@iogates.com>
2017-03-23 14:00:37 +0100

    Added MediaLooks modifications to support their native implementation.

--- sockets.js
+++ sockets.js
@@ -12,15 +12,73 @@ module.exports = function (server, config) {
             audio: false
         };
 
+        //set nickname
+        client.on('nickname', function (nickName) {
+            if (!nickName || nickName == client.nickName) return;
+            client.nickName = nickName;
+            console.log('Client Id:' + client.id + ' sets his name to: ' + nickName);
+        });
+
+        //set info
+        client.on('setinfo', function (info) {
+            //Try to parse in case we have a message from native client
+
+            var infoParsed;
+            if (info && typeof info === 'string')
+                infoParsed = JSON.parse(info);
+            else
+                infoParsed = info;
+
+            if (infoParsed) {
+                if (infoParsed.nickname)
+                    client.nickName = infoParsed.nickname;
+                if (infoParsed.mode)
+                    client.mode = infoParsed.mode;
+                if (infoParsed.strongId)
+                    client.strongId = infoParsed.strongId;
+            }
+            console.log('Client Id:' + client.id + ' changed his info');
+        });
+
+        client.on('getroommembers', function () {
+            var result = gatherRoomMembers(client.room);
+            if (result.clients.length > 0)
+                client.emit('roommembers', result);
+        });
+
         // pass a message to another id
         client.on('message', function (details) {
             if (!details) return;
 
+            //Try to parse in case we have a message from native client
+            if (!details.to) {
+                var result = JSON.parse(details);
+                details = result;
+            }
+
             var otherClient = io.to(details.to);
-            if (!otherClient) return;
+            if (!otherClient) // Maybe strong ID?
+            {
+                var thisroom = io.sockets.adapter.rooms[client.room];
+                if (thisroom) {
+                    for (var id in thisroom) {
+                        if (io.sockets.adapter.nsp.connected[id].strongId == details.to) {
+                            otherClient = io.to(id);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!otherClient)
+                return;
 
             details.from = client.id;
+            details.fromStrongId = client.strongId;
+            details.fromNickName = client.nickName;
+            details.fromMode = client.mode;
+            details.fromRoom = client.room;
             otherClient.emit('message', details);
+            console.log('Client Id: ' + client.id + ' sends message to Id: ' + details.to + ' Type: ' + details.type);
         });
 
         client.on('shareScreen', function () {
@@ -41,7 +99,21 @@ module.exports = function (server, config) {
                     type: type
                 });
                 if (!type) {
+                    console.log('Client Id: ' + client.id + ' leaves room: ' + client.room);
                     client.leave(client.room);
+
+                    //Inform other peers about leaving
+                    io.sockets.in(client.room).emit('memberleaved', {
+                        id: client.id,
+                        strongId: client.strongId,
+                        name: client.nickName,
+                        mode: client.mode
+                    });
+
+                    var result = gatherRoomMembers(client.room);
+                    if (result.clients.length > 0)
+                        io.sockets.in(client.room).emit('roommembers', result);
+
                     client.room = undefined;
                 }
             }
@@ -57,10 +129,27 @@ module.exports = function (server, config) {
                 return;
             }
             // leave any existing rooms
-            removeFeed();
+            if (client.room != name)
+                removeFeed();
+
             safeCb(cb)(null, describeRoom(name));
+
+            //Inform other peers about joining
+            io.sockets.in(name).emit('memberjoined', {
+                id: client.id,
+                strongId: client.strongId,
+                name: client.nickName,
+                mode: client.mode
+            });
+
             client.join(name);
             client.room = name;
+
+            var result = gatherRoomMembers(client.room);
+            if (result.clients.length > 0)
+                io.sockets.in(client.room).emit('roommembers', result);
+
+            console.log('Client Id: ' + client.id + ' joins room: ' + name);
         }
 
         // we don't want to pass "leave" directly because the
@@ -85,6 +174,7 @@ module.exports = function (server, config) {
             if (room && room.length) {
                 safeCb(cb)('taken');
             } else {
+                console.log('Room created: ' + name);
                 join(name);
                 safeCb(cb)(null, name);
             }
@@ -120,7 +210,10 @@ module.exports = function (server, config) {
                 });
             });
         }
+        var conDetails = [client.id, client.handshake.issued + ''];
         client.emit('turnservers', credentials);
+        client.emit('loggedin', conDetails);
+        console.log('Client Id: ' + client.id + ' Connected to signaling');
     });
 
 
@@ -132,7 +225,32 @@ module.exports = function (server, config) {
         };
         Object.keys(clients).forEach(function (id) {
             result.clients[id] = adapter.nsp.connected[id].resources;
+            result.clients[id].strongId = adapter.nsp.connected[id].strongId;
+            result.clients[id].nickName = adapter.nsp.connected[id].nickName;
+            result.clients[id].mode = adapter.nsp.connected[id].mode;
         });
+        return result;
+    }
+
+    function gatherRoomMembers(room) {
+        var result = {
+            clients: []
+        };
+        var thisroom = io.sockets.adapter.rooms[room];
+        if (thisroom) {
+            for (var id in thisroom) {
+                var clName = "undefined";
+                var clMode = "undefined";
+                var clStrongId = "";
+                if (io.sockets.adapter.nsp.connected[id].strongId)
+                    clStrongId = io.sockets.adapter.nsp.connected[id].strongId;
+                if (io.sockets.adapter.nsp.connected[id].nickName)
+                    clName = io.sockets.adapter.nsp.connected[id].nickName;
+                if (io.sockets.adapter.nsp.connected[id].mode)
+                    clMode = io.sockets.adapter.nsp.connected[id].mode;
+                result.clients.push({ id: id, strongId: clStrongId, name: clName, mode: clMode });
+            }
+        }
         return result;
     }
 
@@ -140,6 +258,10 @@ module.exports = function (server, config) {
         return io.sockets.clients(name).length;
     }
 
+    function emitToAllButMe(name, value) {
+
+        return io.sockets.clients(name).length;
+    }
 };
 
 function safeCb(cb) {
